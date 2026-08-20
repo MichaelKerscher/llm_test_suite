98e6a9291c98d06b36518d83805e230804ccc709
michael_k <michael.kerscher@outlook.com>
2026-08-20 10:47:04 +0200

    remove old stuff from project 1 and 2 with gemini and vertex ai evaluation. not part of the final llm-test-suite of the master thesis

--- lib/clients/__init__.py
+++ lib/clients/__init__.py
@@ -1,10 +1,8 @@
 # lib/clients/__init__.py
 
-from lib.clients import gemini_client
 from lib.clients import companygpt_client
 
 CLIENTS = {
-    "gemini": gemini_client,
     "506": companygpt_client,
     "companygpt": companygpt_client
 }
